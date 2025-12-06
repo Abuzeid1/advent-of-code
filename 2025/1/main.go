5bddc6a317e0b88b6e9abd63db42698fe7c98558
Abuzeid <abuzeid1999@gmail.com>
2025-12-06 10:57:48 +0200

    2025, day 1, puzzle 2

--- 2025/1/main.go
+++ 2025/1/main.go
@@ -12,23 +12,46 @@ func main() {
 	if err != nil {
 		fmt.Println("Error reading file: ", err)
 	}
-	DialLeftPointingAtZeroCount := 0
+	dialLeftPointingAtZeroCount := 0
+	dialPointingAtZeroCount := 0
+
 	dialPoint := 50
+
 	for line := range strings.SplitSeq(string(input), "\n") {
 		rotationsNumber, err := strconv.Atoi(line[1:])
 		if err != nil {
 			fmt.Println("Error converting number", err)
 		}
+
 		if line[:1] == "L" {
 			rotationsNumber *= -1
 		}
-
 		dialPoint += rotationsNumber
+
 		if dialPoint%100 == 0 {
-			DialLeftPointingAtZeroCount++
+			dialLeftPointingAtZeroCount++
+		}
+
+		if dialPoint >= 100 {
+			dialPointingAtZeroCount += dialPoint / 100
+			dialPoint %= 100
+		} else if dialPoint < 0 {
+			if dialPoint-rotationsNumber != 0 {
+				dialPointingAtZeroCount++
+			}
+
+			dialPointingAtZeroCount -= dialPoint / 100
+
+			dialPoint %= 100
+			if dialPoint < 0 {
+				dialPoint += 100
+			}
+
+		} else if dialPoint == 0 {
+			dialPointingAtZeroCount++
 		}
 
 	}
-	fmt.Println("First Puzzle Answer", DialLeftPointingAtZeroCount)
-
+	fmt.Println("First Puzzle Answer", dialLeftPointingAtZeroCount)
+	fmt.Println("Second Puzzle Answer", dialPointingAtZeroCount)
 }
